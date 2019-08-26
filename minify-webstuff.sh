fa1fbc49e5c3fdad30803b68e890b8c1b527d3ed
Jonas Hess <jonas.hess@mailbox.org>
2019-08-26 22:03:24 +0200

    added a minifyer (via sed) for *.html-files

--- minify-webstuff.sh
+++ minify-webstuff.sh
@@ -4,6 +4,14 @@
 aminify=(`find ./files/www/ -name "*.css" -o -name "*.js"`)
 
 for minifyme in "${aminify[@]}"; do
-    echo "$minifyme"
+    echo "- minifying: $minifyme"
     yui-compressor $minifyme -o $minifyme
 done
+
+aminify=(`find ./files/www/ -name "*.html"`)
+for minifyme in "${aminify[@]}"; do
+    echo "- minifying: $minifyme"
+    sed -i ':a;N;$!ba;s/>\s*</></g' $minifyme
+done
+
+
